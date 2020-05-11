e878f31e3a0a797e979e8139f8634dcf09a04e95
Michelle Lucero <Michelle.Lucero07@myhunter.cuny.edu>
2020-05-11 16:08:48 -0400

    fixed warning

--- examples/with-mongodb/utils/dbConnect.js
+++ examples/with-mongodb/utils/dbConnect.js
@@ -1,18 +1,19 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
-const connection = {};
+const connection = {}
 
 async function dbConnect() {
   if (connection.isConnected) {
-    return;
+    return
   }
 
   const db = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  });
+    useFindAndModify: false,
+  })
 
-  connection.isConnected = db.connections[0].readyState;
+  connection.isConnected = db.connections[0].readyState
   console.log(connection.isConnected)
 }
 
