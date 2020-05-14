25e6d4a30b8f628f579abf7b55fed58134569d7d
liulanz <zhengliulan@gmail.com>
2020-05-14 13:32:51 -0400

    added some comments

--- examples/with-mongodb/utils/dbConnect.js
+++ examples/with-mongodb/utils/dbConnect.js
@@ -1,17 +1,19 @@
+/* This is a database connection function*/
 import mongoose from 'mongoose'
 
-const connection = {}
+const connection = {} /* creating connection object*/ 
 
-async function dbConnect() {
+async function dbConnect() { /* check if we have connection to our databse*/
   if (connection.isConnected) {
     return
   }
 
+  /* connecting to our database */
   const db = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-  })
+  });
 
   connection.isConnected = db.connections[0].readyState
   console.log(connection.isConnected)
